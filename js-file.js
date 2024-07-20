e681536e0fd06a3906fe498bd59436a2612e93ee
Quinn <qmierlak@gmail.com>
2024-07-20 09:26:17 -0500

    Add 16x16 grid of cells

--- js-file.js
+++ js-file.js
@@ -1,7 +1,28 @@
-// create 16 div squares
-let containerLink = document.querySelector(".container");
+/* create 16 div squares
 for (let i = 0; i < 16; i++) {
     const square = document.createElement('div');
     square.setAttribute("class", "square");
     containerLink.appendChild(square);
+}
+*/
+
+
+/*
+    Strategy:
+    Create 16 rows of 16 grids per row, flexbox row them.
+*/
+
+let containerLink = document.querySelector(".container");
+
+
+for (let row = 0; row < 16; row++) {
+    let rowDiv = document.createElement('div');
+    rowDiv.setAttribute("class", "row");
+
+    for (let i = 0; i < 16; i++) {
+        const cellDiv = document.createElement('div');
+        cellDiv.setAttribute("class", "cell");
+        rowDiv.appendChild(cellDiv);
+    }
+    containerLink.appendChild(rowDiv);
 }
